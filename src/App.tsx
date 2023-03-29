e252c42ec79fcd042b62d63aacb743671d37d087
Liliana Lessa <24816655+LilianaLessa@users.noreply.github.com>
2023-03-29 06:50:37 +0200

    minimal app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => (
+    <View style={{ marginTop: 50 }}>
+        <Text>Hello Expo!</Text>
+    </View>
+);
+
+
+export default App;
