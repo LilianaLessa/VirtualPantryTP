48f087d09e0b3297a604d08c0e672e0227f59a34
Liliana Lessa <24816655+LilianaLessa@users.noreply.github.com>
2023-03-29 17:42:59 +0200

    tmp add product button

--- src/features/products/screens/product.screen.tsx
+++ src/features/products/screens/product.screen.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { View } from "react-native";
-
+import { TouchableOpacity, View } from "react-native";
+import { Button } from "react-native-paper";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Ionicons } from "@expo/vector-icons";
 import ProductList from "../components/product-list.component";
 import ProductSearchBar from "../components/product-search-bar.component";
 
@@ -8,6 +10,12 @@ function ProductScreen() {
   return (
     <View>
       <ProductSearchBar />
+      <TouchableOpacity>
+        <Button mode="elevated">
+          <Ionicons name="md-add" />
+          TMP add product
+        </Button>
+      </TouchableOpacity>
       <ProductList />
     </View>
   );
