ac6f0c55cc854c6b8b7ec65dab468326e186b5af
Liliana Lessa <24816655+LilianaLessa@users.noreply.github.com>
2023-03-30 14:09:21 +0200

    product deletion confirm modal working. need to fix the style, though.

--- src/features/products/components/product-list-item.component.tsx
+++ src/features/products/components/product-list-item.component.tsx
@@ -1,5 +1,7 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import Modal from "react-native-modal";
 
 import {
   MaterialCommunityIcons,
@@ -63,7 +65,136 @@ const styles = StyleSheet.create({
     padding: 0,
     paddingRight: 10,
   },
+  confirmModal1: {
+    top: 0,
+    left: 0,
+    position: "absolute",
+    backgroundColor: "rgba(0,0,0,0.5)",
+    right: 0,
+    bottom: 0,
+  },
+  confirmDialog1: {
+    height: 150,
+    width: 200,
+    shadowColor: "rgba(0,0,0,1)",
+    shadowOffset: {
+      width: 3,
+      height: 3,
+    },
+    elevation: 15,
+    shadowOpacity: 1,
+    shadowRadius: 5,
+    marginTop: 247,
+    marginLeft: 80,
+  },
 });
+
+const modalStyles = StyleSheet.create({
+  container: {
+    backgroundColor: "#E6E6E6",
+    borderWidth: 0,
+    borderColor: "#000000",
+    borderRadius: 20,
+  },
+  dialogTitleContainer: {
+    backgroundColor: "#E6E6E6",
+    alignSelf: "stretch",
+    // flex: 0.1,
+    margin: 15,
+    marginRight: 15,
+    marginLeft: 15,
+  },
+  dialogTitle: {
+    // fontFamily: "open-sans-500",
+    color: "#121212",
+    width: 179,
+    height: 32,
+    fontSize: 18,
+  },
+  dialogContentContainer: {
+    backgroundColor: "#E6E6E6",
+    alignSelf: "stretch",
+    // flex: 0.82,
+    margin: 15,
+    marginTop: 5,
+    padding: 0,
+    paddingTop: 0,
+  },
+  dialogContent: {
+    // fontFamily: "open-sans-500",
+    color: "#121212",
+    fontSize: 12,
+    width: 170,
+    // flex: 1,
+    marginBottom: -11,
+    marginTop: 11,
+    alignSelf: "center",
+  },
+  dialogButtonsContainer: {
+    backgroundColor: "#E6E6E6",
+    alignSelf: "stretch",
+    // flex: 0.08,
+    margin: 15,
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  confirm: {
+    // fontFamily: "open-sans-500",
+    color: "rgba(186,0,13,1)",
+    fontSize: 10,
+  },
+  cancel: {
+    // fontFamily: "open-sans-500",
+    color: "#121212",
+    fontSize: 10,
+  },
+});
+
+function ConfirmDialog({
+  confirmHandler,
+  cancelHandler,
+  dialogTitle,
+  dialogContent,
+  confirm,
+  cancel,
+}: {
+  confirmHandler: () => void;
+  cancelHandler: () => void;
+  dialogTitle?: string;
+  dialogContent?: string;
+  confirm?: string;
+  cancel?: string;
+}) {
+  return (
+    <View style={styles.confirmDialog1}>
+      <View style={modalStyles.dialogTitleContainer}>
+        <Text style={modalStyles.dialogTitle}>
+          {dialogTitle || "Dialog Title"}
+        </Text>
+      </View>
+      <View style={modalStyles.dialogContentContainer}>
+        <Text style={modalStyles.dialogContent}>
+          {dialogContent || "Dialog Content"}
+        </Text>
+      </View>
+      <View style={modalStyles.dialogButtonsContainer}>
+        <TouchableOpacity onPress={confirmHandler}>
+          <Text style={modalStyles.confirm}>{confirm || "Confirm"}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={cancelHandler}>
+          <Text style={modalStyles.cancel}>{cancel || "Cancel"}</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+ConfirmDialog.defaultProps = {
+  dialogTitle: "Dialog Title",
+  dialogContent: "Dialog Content",
+  confirm: "Confirm",
+  cancel: "Cancel",
+};
 
 function ProductListItem({
   item,
@@ -72,6 +203,9 @@ function ProductListItem({
   item: IProduct;
   deleteProductCallback: (productToDelete: IProduct) => void;
 }) {
+  const [isModalVisible, setIsModalVisible] = React.useState(false);
+
+  const handleModal = () => setIsModalVisible(() => !isModalVisible);
   const handleSelfDelete = () => {
     deleteProductCallback(item);
   };
@@ -83,7 +217,7 @@ function ProductListItem({
         <Text style={styles.label}>{item.name}</Text>
       </View>
       <View style={styles.rightContent}>
-        <TouchableOpacity onPress={handleSelfDelete}>
+        <TouchableOpacity onPress={handleModal}>
           <Entypo name="trash" style={styles.tmpRemoveProductIcon} />
         </TouchableOpacity>
         <TouchableOpacity>
@@ -91,6 +225,18 @@ function ProductListItem({
         </TouchableOpacity>
         <MaterialIcons name="drag-handle" style={styles.draggableIcon} />
       </View>
+      <Modal isVisible={isModalVisible}>
+        <View style={styles.confirmModal1}>
+          <ConfirmDialog
+            confirm="Remover"
+            cancel="Cancelar"
+            dialogContent={`Deseja remover o produto '${item.name}'?`}
+            dialogTitle="Remover Produto"
+            cancelHandler={handleModal}
+            confirmHandler={handleSelfDelete}
+          />
+        </View>
+      </Modal>
     </View>
   );
 }
